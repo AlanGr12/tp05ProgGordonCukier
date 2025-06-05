70301045cf39cf2c4be218630305dcaa81e7ec3e
ALAN GABRIEL GORDON <48860730@ort>
2025-06-05 15:23:32 -0300

    obj

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,8 @@ using tp05Gordon_Cukier.Models;
 namespace tp05Gordon_Cukier.Controllers;
 
 public class HomeController : Controller
-{
+{  
+    private const string SessionJuegoKey = "Juego";
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
@@ -13,12 +14,38 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+    public IActionResult introduccion()
     {
-        return View();
+        return View("introduccion");
     }
 
-    public IActionResult salas(){
-        
+    public IActionResult segundaSala()
+    {
+        return View("segundaSala");
+    }
+
+    public IActionResult tercerSala(){
+        return View("terceraSala");
+    }
+
+    [HttpGet]
+
+    public IActionResult Validacion(){
+        return View("validacion");
+    }
+
+    [HttpPost]
+
+    public IActionResult Validacion(string nombreJugador){
+
+        if(nombreJugador != null && nombreJugador != "")
+        HttpContent.Session.SetString("nombreJugador", nombreJugador);
+        HttpContent.Session.SetInt32("HabitacionAct", 1);
+
+        ViewBag.nombreJugador = nombreJugador;
+        ViewBag.numeroHabitacion = 1;
+        return View("Habitacion");
+
+
     }
 }
