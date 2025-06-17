0b0891b9cb46ea47c86f76a1c4363077f1ce64d3
ALAN GABRIEL GORDON <48860730@ort>
2025-06-17 12:01:35 -0300

    listo primera sala

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
 
 // Add services to the container.
@@ -6,11 +8,10 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,6 +19,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession(); 
 
 app.UseAuthorization();
 
